ede78eb5fd0618add165f1113403f755816c676b
tech-atharva <gatly0145@gmail.com>
2020-09-20 12:53:50 +0530

    Updates

---
+++ precache-manifest.47af5cdaf024d7f9193f73c2f497ce71.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c73b34464a395947b81788e7336867",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "e2727041aa730a26579a",
+    "url": "/portfolio-react/static/css/main.4e903c8c.chunk.css"
+  },
+  {
+    "revision": "d54f7c5d045500bc3491",
+    "url": "/portfolio-react/static/js/2.7557c004.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.7557c004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2727041aa730a26579a",
+    "url": "/portfolio-react/static/js/main.61274010.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "f863190bf4ce8f3251774f8e2e389a1a",
+    "url": "/portfolio-react/static/media/1.f863190b.svg"
+  },
+  {
+    "revision": "c281f37762fa6ea638b529448b773cfa",
+    "url": "/portfolio-react/static/media/2.c281f377.svg"
+  },
+  {
+    "revision": "f6396cd4c93316f0da47723fe7f01315",
+    "url": "/portfolio-react/static/media/3.f6396cd4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.63346254fccd3cdeda1a79141c71ba34.js"
+  "/portfolio-react/precache-manifest.47af5cdaf024d7f9193f73c2f497ce71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
